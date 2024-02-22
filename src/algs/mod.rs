a267a7595b96e5e2a15fd9e8711e08e0373426cc
Marcus Henrik Simonsen <marcus@itis.dk>
2024-02-22 14:12:36 +0100

    Interface for nsw

--- src/algs/mod.rs
+++ src/algs/mod.rs
@@ -1,3 +1,8 @@
 pub mod nsw;
 
 pub use crate::nsw::*;
+
+pub trait KNNS<T> {
+    fn search(&self, q: T, k: usize) -> impl Iterator<Item = &T>;
+    fn insert(&mut self, p: T, f: i32, w: i32);
+}
--- src/algs/nsw.rs
+++ src/algs/nsw.rs
@@ -1,5 +1,13 @@
 use crate::SimpleGraph;
 
 pub struct NSW<T> {
-    graph: SimpleGraph<T>
+    graph: SimpleGraph<T>,
+    ef: usize,
+    ep: &T,
+}
+
+impl<T> KNNS<T> for NSW<T> {
+    fn search(&self, q: T, k: usize) -> impl Iterator<Item = &T> {
+        let mut visited = 
+    }
 }
